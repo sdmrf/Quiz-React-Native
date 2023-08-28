00ed4823de963493c54a268b7069845d32650242
Syed Maruf Ali <syedmarufali23@gmail.com>
2023-08-28 23:18:06 +0530

    Project Initialized

---
+++ quizi/Screens/Quiz.js
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class Quiz extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Quiz</Text>
+      </View>
+    )
+  }
+}
+
+export default Quiz
---
+++ quizi/Screens/Result.js
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class Result extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Result</Text>
+      </View>
+    )
+  }
+}
+
+export default Result
