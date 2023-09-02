35502bf1ece10dec68766b1f3724820b6e3d5b8a
Syed Maruf Ali <syedmarufali23@gmail.com>
2023-09-03 00:24:08 +0530

    Updated

--- quizi/Screens/Quiz.js
+++ quizi/Screens/Quiz.js
@@ -1,11 +1,20 @@
-import { StyleSheet, Text, View } from "react-native";
-import * as React from "react";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TouchableHighlight } from "react-native";
 import { ProgressBar, MD3Colors } from "react-native-paper";
 
 const Category = "Sports";
 const CurrQue = 5;
+const Question =
+  "What caused Jake Lloyd who played Anakin Skywalker in The Phantom Menace to quit acting?";
+const Options = ["Option 1", "Option 2", "Option 3", "Option 4"];
 
 const Quiz = () => {
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleOptionPress = (option) => {
+    setSelectedOption(option);
+  };
+
   return (
     <View style={styles.Quiz}>
       <View style={styles.QuizTop}>
@@ -24,10 +33,46 @@ const Quiz = () => {
         </View>
       </View>
       <View style={styles.QuizMid}>
-        <Text>Quiz</Text>
+        <View style={styles.QuizMidQue}>
+          <Text style={styles.Question}>{Question}</Text>
+          <View style={styles.Options}>
+            {Options.map((option, index) => (
+              <TouchableHighlight
+                key={index}
+                style={[
+                  styles.Option,
+                  {
+                    backgroundColor:
+                      selectedOption === option
+                        ? "rgba(85, 110, 255, 0.5)"
+                        : "rgba(255, 255, 255, 1)",
+                  },
+                ]}
+                underlayColor="rgba(85, 110, 255, 0.8)"
+                onPress={() => handleOptionPress(option)}
+              >
+                <Text
+                  style={[
+                    styles.OptionText,
+                    {
+                      color: selectedOption === option ? "#FFFFFF" : "#556bf4",
+                    },
+                  ]}
+                >
+                  {option}
+                </Text>
+              </TouchableHighlight>
+            ))}
+          </View>
+        </View>
       </View>
       <View style={styles.QuizBottom}>
-        <Text>Quiz</Text>
+        <View style={styles.BottomQuit}>
+          <Text style={styles.QuitText}>Quit</Text>
+        </View>
+        <View style={styles.BottomNext}>
+          <Text style={styles.NextText}>Next</Text>
+        </View>
       </View>
     </View>
   );
@@ -55,7 +100,7 @@ const styles = StyleSheet.create({
     flex: 1,
     width: "100%",
     height: "100%",
-    backgroundColor: "rgba($color: #556bf4, $alpha: 0.75)",
+    backgroundColor: "rgba(85, 110, 255, 0.25)",
     borderRadius: 15,
     border: "1px solid #FFFFFF",
     padding: 10,
@@ -89,20 +134,74 @@ const styles = StyleSheet.create({
     height: "100%",
     alignItems: "center",
     justifyContent: "center",
-    padding: 15,
+    padding: 10,
     alignItems: "center",
     justifyContent: "center",
   },
   CurrQue: {
     fontSize: 25,
-    fontWeight: "bold",
+    fontWeight: "500",
     color: "#F2F2F2",
   },
 
   QuizMid: {
-    flex: 3,
+    flex: 4,
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  QuizMidQue: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
+  },
+  Question: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#F2F2F2",
+    textShadowColor: "rgba(0, 0, 0, 0.25)",
+  },
+  Options: {
+    flex: 4,
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 10,
+    padding: 10,
+    gap: 10,
+    
+  },
+  Option: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    backgroundColor: "rgba(255, 255, 255, 1)",
+    borderRadius: 15,
+    border: "1px solid #FFFFFF",
+    padding: 10,
+    boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+    textShadowColor: "rgba(0, 0, 0, 0.25)",
+    alignItems: "center",
+    justifyContent: "center",
+    
+  },
+  OptionText: {
+    fontSize: 25,
+    fontWeight: "700",
+    color: "rgba(85, 110, 255, 1)",
   },
   QuizBottom: {
     flex: 1,
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 10,
+    flexDirection: "row",
   },
 });
